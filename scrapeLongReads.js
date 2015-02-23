93156ff526bb8a718fbaa7324dff43e4ff382222
Aurora Nou <auroranou@gmail.com>
2015-02-23 11:11:41 -0500

    fix file writing problem

--- scrapeLongReads.js
+++ scrapeLongReads.js
@@ -6,10 +6,13 @@ var http = require('http');
 var body = '';
 var longReads = [];
 
-for (var i = 3; i < 6; i++) {
+// async.eachSeries(array, iterator, callback);
+// var arr = [99,78,100,456,560];
+var arr = [1,2,3,99,78,212,350,456,560]
+async.eachSeries(arr, function(i, callback){
 	getLongReads(i);
-}
-// getLongReads(5);
+	callback();
+});
 
 function getLongReads(pageNum) {
 	http.get('http://longreads.com/articles/search/?q=&page=' + pageNum, function(response) {
@@ -20,38 +23,52 @@ function getLongReads(pageNum) {
 			var $ = cheerio.load(body);
 			$('div.article').each(function(i) {
 				var title = $(this).find('a.article-title').text();
-				title = title.trim().replace(/(^\s+|\s+$)/g,'');
-				// console.log(i, title);
+				// console.log(title);
 				var articleUrl = $(this).find('a.article-title').attr('href');
 				// console.log(articleUrl);
-				var author = $(this).find('div.article_details_left').children().first().text();
-				author = author.replace(/[^\s]*\s/, '');
+				var author = $(this).find('div.article_details_left div:first-child').text();
 				// console.log(author);
-				var source = $(this).find('div.article_details_left').children().last().text();
-				source = source.replace(/[^\s]*\s/, '');
+				var source = $(this).find('div.article_details_left div:nth-child(2)').text();
 				// console.log(source);
-				var pubDate = $(this).find('div.article_details_right').children().first().text();
-				pubDate = pubDate.trim().replace(/[^\s]*\s+/, '');
+				var pubDate = $(this).find('div.article_details_right div:nth-child(1)').text();
 				// console.log(pubDate);
-				var length = $(this).find('div.article_details_right').children().last().text();
-				minuteLength = length.trim()
-					.replace(/\([^)]*\)/, '')
-					.replace(/[^\d]+/, '');
-				// console.log(minuteLength);
-				wordLength = length.match(/\(([^)]+)\)/)[1];
-				// console.log(wordLength);
+				var length = $(this).find('div.article_details_right div:nth-child(2)').text();
+				// console.log(length)
 				longReads.push({
-					title: title,
+					title: formatTitle(title),
 					articleUrl: articleUrl,
-					author: author,
-					source: source,
-					pubDate: pubDate,
-					minuteLength: minuteLength,
-					wordLength: wordLength
+					author: formatGeneral(author),
+					source: formatGeneral(source),
+					pubDate: formatGeneral(pubDate),
+					minuteLength: getMinuteLength(length),
+					wordLength: getWordLength(length)
 				});
 			});
-			fs.writeFileSync('longReads.json', JSON.stringify(longReads));
-			// console.log(longReads);
+			// db.longreadsdb.insert(longReads);
 		});
+		fs.writeFileSync('longReads.json', JSON.stringify(longReads));
+		console.log(longReads.length);
 	});
+}
+
+function formatTitle(str) {
+	return str.trim().replace(/(^\s+|\s+$)/g,'');
+}
+
+function formatGeneral(str) {
+	return str.trim().replace(/[^\s]*\s/, '');
+}
+
+function getMinuteLength(str) {
+	return str.replace(/\([^)]*\)/, '')
+		.replace(/[^\d]+/, '')
+		.trim();
+}
+
+function getWordLength(str) {
+	if (str.match(/\(([^)]+)\)/)) {
+		return str.match(/\(([^)]+)\)/)[1];
+	} else {
+		return null;
+	}
 }
